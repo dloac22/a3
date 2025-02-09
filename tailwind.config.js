06b5bd477ae5578737b5dee98823700fc6d98153
Dat The Loac <dloac@wpi.edu>
2025-02-09 12:12:58 -0500

    added TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	content: [
+	  "./public/*.html",
+	  "./public/*.js"
+	],
+	theme: {
+	  extend: {
+		fontFamily: {
+		  'sans': ['Poppins', 'sans-serif']
+		}
+	  }
+	},
+	plugins: [],
+  }
